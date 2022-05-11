740e6ba863133ca71ebbd151857b6c4910ce5dd0
Atul Singh <atul19971@gmail.com>
2022-05-11 02:31:00 -0400

    Update RewardCalculator.java

--- src/main/java/com/reward/calculator/util/RewardCalculator.java
+++ src/main/java/com/reward/calculator/util/RewardCalculator.java
@@ -19,7 +19,7 @@ public class RewardCalculator {
 	 */
 	public int calculateReward(float transactionValue) {
 		int hundredAbove = Math.max(0, (int) transactionValue - 100);
-		int fiftyAbove = Math.max(0, (int) transactionValue - 50);
+		int fiftyAbove = Math.max(0, ((int) transactionValue - 50) > 50 ? 50 : (int) transactionValue - 50);
 		return hundredAbove * 2 + fiftyAbove * 1;
 	}
 }
